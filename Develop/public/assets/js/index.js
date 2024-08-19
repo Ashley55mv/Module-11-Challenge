2fcf79f37f4216a34c1df8226e9353d72b9a99aa
Ashley Cowan <ashley.m.cowan@gmail.com>
2024-08-19 23:29:22 +1000

    Updated file structure

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // HTML Routes
-app.get('*', (req, res) => {
+app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
